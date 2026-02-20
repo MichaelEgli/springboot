0d79b085beddac9dff441a93b3073580240f49b8
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2026-02-20 15:07:01 +0100

    add employee entity, repo and unit test

--- 03_my_exercise_persistence/src/test/java/ch/eglim/springboot/exercise/persistence/PersistenceApplicationTests.java
+++ 03_my_exercise_persistence/src/test/java/ch/eglim/springboot/exercise/persistence/PersistenceApplicationTests.java
@@ -1,6 +1,7 @@
 package ch.eglim.springboot.exercise.persistence;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
@@ -8,8 +9,22 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class PersistenceApplicationTests {
 
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
 	@Test
-	void contextLoads() {
+	void addEmployee() {
+
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("John Doe");
+        employee.setSalary(50000);
+
+        employeeRepo.save(employee);
+
+        Employee savedEmployee = employeeRepo.findById(1).orElseThrow();
+        System.out.println("Saved Employee: " + savedEmployee.getName() + ", Salary: " + savedEmployee.getSalary());
+
         System.out.println("Breakpoint here to access manually to http://localhost:8080/h2-console (User sa, pw <leer>");
 	}
 

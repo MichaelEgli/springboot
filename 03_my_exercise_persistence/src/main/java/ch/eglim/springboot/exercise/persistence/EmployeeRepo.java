f63da2a514af0f152ebe678e0f7fa6a3fa4060c5
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2026-02-25 16:05:04 +0100

    use jpa repo

--- 03_my_exercise_persistence/src/main/java/ch/eglim/springboot/exercise/persistence/EmployeeRepo.java
+++ 03_my_exercise_persistence/src/main/java/ch/eglim/springboot/exercise/persistence/EmployeeRepo.java
@@ -1,9 +1,9 @@
 package ch.eglim.springboot.exercise.persistence;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepo extends CrudRepository<Employee,Integer> {
+public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
 
 }

b7c848a5a359d2a4412ffb7d20a92e1038acb19b
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2026-03-06 16:06:09 +0100

    Add comments to addAndRemoveEmployee test method

--- 03_my_exercise_persistence/src/test/java/ch/eglim/springboot/exercise/persistence/PersistenceApplicationTests.java
+++ 03_my_exercise_persistence/src/test/java/ch/eglim/springboot/exercise/persistence/PersistenceApplicationTests.java
@@ -16,7 +16,8 @@ class PersistenceApplicationTests {
 
 	@Test
 	void addAndRemoveEmployee() {
-
+        
+        // add Employee
         Employee employee = new Employee();
         employee.setId(1);
         employee.setName("John Doe");
@@ -26,14 +27,13 @@ class PersistenceApplicationTests {
 
         Employee savedEmployee = employeeRepo.findById(1).orElseThrow();
         System.out.println("Saved Employee: " + savedEmployee.getName() + ", Salary: " + savedEmployee.getSalary());
-
         System.out.println("Breakpoint here to access manually to http://localhost:8080/h2-console (User sa, pw <leer>");
 
+        // remove Employee
         Employee removedEmployee = employeeRepo.findById(1).orElseThrow();
         employeeRepo.delete(removedEmployee);
 
         // Prüfen, dass keine Einträge mehr vorhanden sind
         assertTrue(employeeRepo.findAll().isEmpty(), "EmployeeRepo sollte leer sein");
 	}
-
 }

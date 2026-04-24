704fb276186987b4544639462dfa71c63e9dd7bc
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2026-04-24 15:47:49 +0200

    add JpaSpecificationExecutor<Employee> to employeeRepo

--- 03_my_exercise_persistence/src/main/java/ch/eglim/springboot/exercise/persistence/EmployeeRepo.java
+++ 03_my_exercise_persistence/src/main/java/ch/eglim/springboot/exercise/persistence/EmployeeRepo.java
@@ -1,9 +1,11 @@
 package ch.eglim.springboot.exercise.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
+public interface EmployeeRepo extends JpaRepository<Employee,Integer>, JpaSpecificationExecutor<Employee> {
 
 }

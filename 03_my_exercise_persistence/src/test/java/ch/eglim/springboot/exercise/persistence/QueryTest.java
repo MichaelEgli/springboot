b1e5b04cdc63e49890c28a89e7f57d928995da7a
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2026-06-26 15:10:49 +0200

    QueryTest - add ex1

--- 03_my_exercise_persistence/src/test/java/ch/eglim/springboot/exercise/persistence/QueryTest.java
+++ 03_my_exercise_persistence/src/test/java/ch/eglim/springboot/exercise/persistence/QueryTest.java
@@ -1,1 +1,41 @@
+package ch.eglim.springboot.exercise.persistence;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Import(TestcontainersConfiguration.class)
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
+public class QueryTest {
+
+    @Autowired
+    private EntityManager em;
+    @Autowired
+    private EmployeeRepo employeeRepository;
+    @Autowired
+    private DepartmentRepo departmentRepository;
+
+    /**
+     * Ex1	Find all employees who live in the canton of Zurich
+     */
+    @Test
+    @Sql("/db/migration/afterMigrate.sql")
+    void findAllZuercher() {
+        TypedQuery<Employee> query = em.createQuery(
+                "select e from Employee e where e.address.state = 'ZH'", Employee.class);
+        List<Employee> zuercher = query.getResultList();
+
+        assertEquals(3, zuercher.size());
+    }
+}

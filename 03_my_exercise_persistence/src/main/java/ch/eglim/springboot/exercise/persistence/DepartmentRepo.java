a19e48c86bb4ea5dc85d67677e1ea2e4423ac222
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2026-06-29 13:21:32 +0200

    Query Test - ex2 - namedQuery Version

--- 03_my_exercise_persistence/src/main/java/ch/eglim/springboot/exercise/persistence/DepartmentRepo.java
+++ 03_my_exercise_persistence/src/main/java/ch/eglim/springboot/exercise/persistence/DepartmentRepo.java
@@ -1,6 +1,11 @@
 package ch.eglim.springboot.exercise.persistence;
 
+import ch.eglim.springboot.exercise.persistence.dto.DepartmentSalaryStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartmentRepo extends JpaRepository<Department, Integer> {
+
+    List<DepartmentSalaryStatistics> avgSalary();
 }
